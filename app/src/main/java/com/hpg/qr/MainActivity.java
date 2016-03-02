f5ca2e90fc522fc86b93db0122c0e87ce1dd9687
Paul <huangpg@59store.com>
2016-03-02 17:20:39 +0800

    first commit

---
+++ app/src/main/java/com/hpg/qr/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hpg.qr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
